f5710f93bd9a7b6c2ca99d63f8fb8c1838606a69
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2020-10-26 15:05:22 +0100

    final

---
+++ Canon/js/map.js
@@ -0,0 +1,179 @@
+mapboxgl.accessToken = 'Your Token key here!'; // This Token Expire in 24 hours
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+        var x = 2;
+        if (position.coords.longitude > 10.757933 && position.coords.longitude < 30.8778364) {
+            x = 8;
+        } else {
+            x = 2;
+        }
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/dark-v10',
+            center: [10.757933, 59.911491],
+
+            zoom: x
+
+        });
+
+
+        var destination = [10.757933, 59.911491];
+
+
+        var origin = [position.coords.longitude, position.coords.latitude];
+        console.log(origin)
+        console.log(destination)
+        var makrer = new mapboxgl.Marker().setLngLat(origin).addTo(map)
+
+        var makrer = new mapboxgl.Marker().setLngLat(destination).addTo(map)
+        // A simple line from origin to destination.
+        var route = {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+
+                    'geometry': {
+                        'type': 'LineString',
+                        'coordinates': [origin, destination]
+                    }
+
+                }
+            ]
+        };
+
+        // A single point that animates along the route.
+        // Coordinates are initially set to origin.
+        var point = {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': origin
+                    },
+                    'properties': {}
+
+                }
+            ]
+        };
+
+        // Calculate the distance in kilometers between route start/end point.
+        var lineDistance = turf.lineDistance(route.features[0], 'kilometers');
+
+        var arc = [];
+
+        // Number of steps to use in the arc and animation, more steps means
+        // a smoother arc and animation, but too many steps will result in a
+        // low frame rate
+        var steps = 500;
+
+        // Draw an arc between the `origin` & `destination` of the two points
+        for (var i = 0; i < lineDistance; i += lineDistance / steps) {
+            var segment = turf.along(route.features[0], i, 'kilometers');
+            arc.push(segment.geometry.coordinates);
+        }
+
+        // Update the route with calculated arc coordinates
+        route.features[0].geometry.coordinates = arc;
+
+        // Used to increment the value of the point measurement against the route.
+        var counter = 0;
+
+        map.on('load', function () {
+            // Add a source and layer displaying a point which will be animated in a circle.
+            map.addSource('route', {
+                'type': 'geojson',
+                'data': route
+            });
+
+            map.addSource('point', {
+                'type': 'geojson',
+                'data': point
+            });
+
+            map.addLayer({
+                'id': 'route',
+                'source': 'route',
+                'type': 'line',
+                'paint': {
+                    'line-width': 3,
+                    'line-color': '#FF00FF'
+                }
+            });
+
+            map.addLayer({
+                'id': 'point',
+                'source': 'point',
+                'type': 'symbol',
+
+                'layout': {
+                    'icon-image': 'airport-15',
+                    'icon-rotate': ['get', 'bearing'],
+                    'icon-rotation-alignment': 'map',
+                    'icon-allow-overlap': true,
+                    'icon-ignore-placement': true,
+
+
+
+
+
+                }
+            });
+
+            function animate() {
+                // Update point geometry to a new position based on counter denoting
+                // the index to access the arc.
+                point.features[0].geometry.coordinates =
+                    route.features[0].geometry.coordinates[counter];
+
+                // Calculate the bearing to ensure the icon is rotated to match the route arc
+                // The bearing is calculate between the current point and the next point, except
+                // at the end of the arc use the previous point and the current point
+                point.features[0].properties.bearing = turf.bearing(
+                    turf.point(
+                        route.features[0].geometry.coordinates[
+                        counter >= steps ? counter - 1 : counter
+                        ]
+                    ),
+                    turf.point(
+                        route.features[0].geometry.coordinates[
+                        counter >= steps ? counter : counter + 1
+                        ]
+                    )
+                );
+
+                // Update the source with this new data.
+                map.getSource('point').setData(point);
+
+                // Request the next frame of animation so long the end has not been reached.
+                if (counter < steps) {
+                    requestAnimationFrame(animate);
+                }
+
+                counter = counter + 1;
+            }
+
+            document
+                .getElementById('map')
+                .addEventListener('click', function () {
+                    // Set the coordinates of the original point back to origin
+                    point.features[0].geometry.coordinates = origin;
+
+                    // Update the source layer
+                    map.getSource('point').setData(point);
+
+                    // Reset the counter
+                    counter = 0;
+
+                    // Restart the animation.
+                    animate(counter);
+                });
+
+            // Start the animation.
+            animate(counter);
+        });
+    });
+}
---
+++ Canon/readme.txt
@@ -0,0 +1,13 @@
+Deployment Package – Frontend Webutvikling - Gruppe 03 - Canon
+Prosjektbeskrivelse
+Canon er en internasjonalt velkjent merkevare. De er mest kjent for utvikling og salg av digitale kameraer og linser, samt printere. Canon er en profesjonell og innovativ bedrift som selger produkter av høy kvalitet. Disse verdiene måtte nettsiden formidle. 
+Noen av produktene til Canon som er verdt å nevne er; Canon EOS speilreflekskameraer, speilløse kameraer og kompakt kameraer. 
+Nettstedet vil være en viktig del av kundereisen for Canon sine kunder. Nettstedet er en plass kunder kan besøke for å finne informasjonen de trenger om produktene. Canon sine produkter har en intensiv distribusjonsgrad. Dette betyr at produktene deres er tilgjengelig og kan kjøpes i alle elektronikk og foto butikker i hele landet. Det er naturlig at en kunde som vurderer å kjøpe et Canon produkt i en fysisk butikk, leter etter mer informasjon om produktet på internett før kunden tar sluttvalget om å kjøpe produktet. Når kunden er i vurderingsfasen i kundereisen og aktivt leter etter god informasjon om produktet han vurderer, er det viktig at Canon kommuniserer denne informasjonen og verdigrunnlagene deres på en måte som er lett å ta til seg. 
+Utviklere
+Abdimalik Ali, Thor Christian Nilsen, Rahmat Mozafari og Marius Karlsen.
+Web design 
+Nettstedets utvikling skjedde i to perioder. I første periode ble HTML og CSS brukt for å implementere nettstedets struktur og utseende. Nettstedet var fungerende, men var ikke responsivt. 
+I andre periode ble nettstedet gjort responsivt og andre elementer som web-slider og geolokasjon ble implementert. Andre mindre endringer for å optimalisere brukervennlighet ble også gjort i denne perioden. 
+Vi delte tidlig arbeidsoppgavene mellom gruppemedlemmene slik at alle var sikre på sine roller i utviklingsprosessen. 
+Teknologi brukt for implementasjon
+HTML5, CSS3, Javascript og Mapbox API
